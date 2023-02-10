7aab76849c0bdab562931a2ee85d33d64a67d604
Xenowa <xenocodecs@gmail.com>
2023-02-10 21:24:54 +0530

    Setting up counters

--- js/custom.js
+++ js/custom.js
@@ -543,4 +543,19 @@ $(function () {
     document.getElementById("contact-trigger").click()
   }
 
+  // -------------------------------------------- //
+  // custom counter
+  // -------------------------------------------- //
+  $('.counter').each(function () {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+
 });
