d73c53c190af82315d57d98ac8ae32917ba40391
Xenowa <xenocodecs@gmail.com>
2023-04-24 09:18:41 +0530

    Adding DesignSprint Submission template

--- js/custom.js
+++ js/custom.js
@@ -257,8 +257,8 @@ $(function () {
         "color": {
           //"value": "#f0ecb2" // IdeaSprint
           // "value": "#5115b2" // DesignSprint
-          "value": "#FF5959" // BuildSprint
-          //"value": "#526CFF" // SprintPitch
+          // "value": "#FF5959" // BuildSprint
+          "value": "#526CFF" // SprintPitch
         },
         "shape": {
           "type": "circle",
